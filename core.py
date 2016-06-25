f08ccc81bde7bcb854aba64f9daadf911c1bf4dc
Etskh <etskh@hotmail.com>
2016-06-25 04:03:14 -0700

    Aligns everything with PEP-8... enough :gun:

--- core.py
+++ core.py
@@ -19,14 +19,20 @@ class Model(object):
 
 
 class Collection(object):
-    "This is a group of models"
+    """
+    This is a group of models
+    """
 
     def __init__(self, model_type):
         self.items = []
         self.model_type = model_type
 
     def load(self, items):
-        "Loads in an array of dictionaries, and creates models out of them"
+        """
+        Loads in an array of dictionaries, and creates models out of them
+        :param items: python array of objects
+        :return: nothing
+        """
         for item in items:
             self.items.append(self.model_type(item))
 
@@ -34,7 +40,12 @@ class Collection(object):
         return self.items[index]
 
     def get_by_field(self, field, value):
-        "Retrieves the first item where its `field` matches its value"
+        """
+        Retrieves the first item where its `field` matches its value
+        :param field: String name of the field to search for (eg: "name")
+        :param value: String value of the field (eg: "George")
+        :return: the matching element or None
+        """
         for item in self.items:
             if item.__dict__[field] == value:
                 return item
@@ -49,17 +60,20 @@ class CannedDataSource(object):
 
     def get(self, model):
         "This retrieves all models of the type model"
-        collection = Collection(model)
+
         if model.name == 'race':
-            with open('data/races.json') as data_file:
-                data = json.load(data_file)
-                collection.load(data)
+            return self.load_from_file(model, 'data/races.json')
         if model.name == 'class':
-            with open('data/classes.json') as data_file:
-                data = json.load(data_file)
-                collection.load(data)
+            return self.load_from_file(model, 'data/classes.json')
         if model.name == 'character':
-            with open('data/characters.json') as data_file:
-                data = json.load(data_file)
-                collection.load(data)
+            return self.load_from_file(model, 'characters/races.json')
+        return None
+
+    def load_from_file(self, model, filename):
+        "Loads a collection of models from a file and returns it"
+
+        collection = Collection(model)
+        with open(filename) as data_file:
+            data = json.load(data_file)
+            collection.load(data)
         return collection
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-
+"""
+The models for the pathfinder widget
+"""
 
 import json
 import math
@@ -7,6 +9,9 @@ from core import Model, Collection, CannedDataSource
 
 
 class SizeModel(Model):
+    """
+    Represents a size-class in the D&D world
+    """
 
     def __init__(self, name):
         self.index = SizeModel._list[name]
@@ -19,27 +24,46 @@ class SizeModel(Model):
 
     @property
     def attack_bonus(self):
+        """
+        :return: The attack bonus granted to a character for its size
+        """
         return self.ac_bonus
 
     @property
     def ac_bonus(self):
+        """
+        :return: The AC bonus granted to a character for its size
+        """
         if self.index - 4 == 0:
             return 0
         return 2 ** (abs(self.index-4)-1) * (-1 if self.index < 4 else 1)
 
     @property
     def hide_bonus(self):
-        self.ac_bonus * 4
+        """
+        :return: The bonus granted to characters to stealth checks
+        """
+        return self.ac_bonus * 4
 
     @property
     def lightest(self):
+        """
+        :return: The lightest weight for this size
+        """
         return 8.0 ** (self.index - 2.0)
 
     @property
     def heaviest(self):
-        return self.getLightest() * 8.0
+        """
+        :return: The heaviest weight
+        """
+        return self.lightest * 8.0
 
     def compute_weight_ratio(self, other_size):
+        """
+        :param other_size: another SizeModel instance
+        :returns: the ratio between this size and another
+        """
         this_delta = self.heaviest - self.lightest
         that_delta = other_size.heaviest - other_size.lightest
         return this_delta / that_delta
@@ -58,12 +82,15 @@ class SizeModel(Model):
 
 
 class RaceModel(Model):
+    """
+    This is the concept of the race
+    """
 
     name = 'race'
 
     def __init__(self, data):
         self.name = data['name']
-        self.size = SizeModel( data['size'])
+        self.size = SizeModel(data['size'])
 
     def __str__(self):
         return str(self.name)
@@ -74,6 +101,9 @@ class RaceModel(Model):
 
 
 class SpellModel(Model):
+    """
+    Spell class to manage casting and effect
+    """
 
     name = 'spell'
 
@@ -89,6 +119,9 @@ class SpellModel(Model):
 
 
 class ClassLevelModel(Model):
+    """
+    Each level in a class has one of these
+    """
     name = 'class-level'
 
     def __init__(self, data):
@@ -104,7 +137,12 @@ class ClassLevelModel(Model):
 
 
 class UnknownRateError(Exception):
+    """
+    Exception that is raised when the value is not one of
+    the specified rates
+    """
     def __init__(self, value):
+        super(UnknownRateError, self).__init__()
         self.value = value
 
     def __str__(self):
@@ -112,7 +150,14 @@ class UnknownRateError(Exception):
 
 
 class ClassModel(Model):
+    """
+    Important class of class
+    """
     name = 'class'
+
+
+    # pylint: disable:invalid-name
+    # Fuck you pylint, I'll call my functions whatever I want
 
     def __init__(self, data):
         self.name = data['name']
@@ -123,13 +168,13 @@ class ClassModel(Model):
         for i in range(1, 20):
             level = {
                 'level': i,
-                'base_attack': ClassModel.get_rate_at_level(data['base_attack_rate'], i),
-                'fort_save': ClassModel.get_rate_at_level(data['fort_save'], i),
-                'ref_save': ClassModel.get_rate_at_level(data['ref_save'], i),
-                'will_save': ClassModel.get_rate_at_level(data['will_save'], i),
+                'base_attack': ClassModel.rate_at_level(data['base_attack_rate'], i),
+                'fort_save': ClassModel.rate_at_level(data['fort_save'], i),
+                'ref_save': ClassModel.rate_at_level(data['ref_save'], i),
+                'will_save': ClassModel.rate_at_level(data['will_save'], i),
             }
             try:
-                level['spd'] = ClassModel.get_spd_at_level(data['spells_per_day_rate'], i)
+                level['spd'] = ClassModel.spd_at_level(data['spells_per_day_rate'], i)
             except KeyError:
                 pass
             level_data.append(level)
@@ -143,9 +188,13 @@ class ClassModel(Model):
             self.bonus_spells_per_day = False
             self.casting_stat = None
 
-
     @staticmethod
-    def get_rate_at_level(rate, level):
+    def rate_at_level(rate, level):
+        """
+        :param rate: one of 'fast', 'average', 'slow', 'strong', or 'weak'
+        :param level: Integer (1-20)
+        :return: the value of the algorithm at the level
+        """
         # Base attack bonuses
         if rate == 'fast':
             return level
@@ -163,6 +212,12 @@ class ClassModel(Model):
 
     @staticmethod
     def wide_spell_count_at_level(char_level, spell_level):
+        """
+        The "wide" spell per day implementation
+        :param char_level: Integer (1-20)
+        :param spell_level: Integer (0-9)
+        :return: an integer of slots of that level per day
+        """
         # =FLOOR(MIN(4,IF(C$2=0,1,0)+LOG(MAX(1,$B7 - (C$2*2)+3)) *4.5))
         log_arg = max(1.0, float(char_level) - (float(spell_level) * 2) + 3)
         real_answer = 1.0 if spell_level == 0 else 0
@@ -171,7 +226,12 @@ class ClassModel(Model):
         return real_answer
 
     @staticmethod
-    def get_spd_at_level(rate, level):
+    def spd_at_level(rate, level):
+        """
+        :param rate: 'wide', 'late'
+        :param level: 1-20
+        :return: a dict of spells per day at a certain level
+        """
 
         if rate == 'wide':
             spd = {}
@@ -184,12 +244,23 @@ class ClassModel(Model):
 
         raise UnknownRateError(rate)
 
+
     def at(self, level):
+        """
+        :param level: Integer
+        :return: the ClassLevelModel instance at that level
+        """
         return self.levels.get_by_field('level', level)
 
 
+class SpellSlot(object):
+    """
+    This is just an object to act as a holder for some data
+    """
 
-class SpellSlot():
+    # pylint: disable=too-few-public-methods
+    # This class will see the light of the sun!
+
     def __init__(self, **kwargs):
 
         self.reason = kwargs['reason']
@@ -202,11 +273,14 @@ class SpellSlot():
             self.school = None
 
 
-
 class CharacterModel(Model):
-
+    """
+    The character class is the master controller class
+    """
     name = 'character'
-    classCollection = []
+
+    # pylint: disable=too-many-instance-attributes
+    # Character is like the master class!
 
     def __init__(self, data):
         self.name = data['name']
@@ -234,27 +308,30 @@ class CharacterModel(Model):
             self.levels[cls] = data['classes'][cls]
             self.classes[cls] = classes.get_by_field('name', cls)
 
-
-        # TODO: Make this into the choice system
         try:
             self.school_specialization = data['school_specialization']
         except KeyError:
             self.school_specialization = None
 
         self.spells_known = data['spells_known']
-        self.spells = []
 
         self._spell_slots = None
 
-
     def get_level_as_string(self):
+        """
+        Returns something along the lines of Wizard 1/Rogue 2"
+        :return: string of class and level
+        """
         levels = []
-        for levelClass in self.levels:
-            levels.append('{0} {1}'.format(levelClass, self.levels[levelClass]))
+        for level_class in self.levels:
+            levels.append('{0} {1}'.format(level_class, self.levels[level_class]))
         return ' / '.join(levels)
 
     @property
     def spell_slots(self):
+        """
+        Property that is an array of spell_slots
+        """
         text = {
             'int': 'smart',
             'wis': 'wise',
@@ -275,13 +352,13 @@ class CharacterModel(Model):
 
                 spell_count = int(class_level.spd[spell_level])
 
-                for x in range(0, spell_count):
+                for i in range(0, spell_count):
 
                     # Add that many spells
                     #
                     self.add_spell_slot(
                        reason='Daily spells per level {} ({} of {})'.format(
-                           spell_level, x, spell_count
+                           spell_level, i, spell_count
                        ),
                        cls=self.classes[cls],
                        level=spell_level
@@ -330,6 +407,13 @@ class CharacterModel(Model):
         return self.add_spell_slot(**kwargs)
 
     def add_spell_slot(self, **kwargs):
+        """
+        Adds a spell slot to the character's list of spells
+        :param kwargs:
+            cls: the class object
+            level: the level of the class
+        :returns: True on adding the spell, and False if it didn't
+        """
 
         casting_stat = kwargs['cls'].casting_stat
         if int(kwargs['level']) + 10 > self.stats[casting_stat]:
@@ -345,37 +429,47 @@ class CharacterModel(Model):
         return True
 
     def __str__(self):
+        "String representation"
         return json.dumps(self.__dict__)
 
     def mod(self, stat):
+        "Returns the modifier for the stat"
         return self.stats[stat] / 2 - 5
 
-    def _getClassValue(self, valueName):
+    def _get_class_value(self, value_name):
+        """
+        Obtains the value for a field within all classes and sums them
+        """
         val = 0
-        for cls in self.levels:
+        for cls in self.classes:
             level = self.levels[cls]
-            classModel = CharacterModel.classCollection.get_by_field('name', cls)
-            val += classModel.atLevel(level).__dict__[valueName]
+            val += self.classes[cls].at_level(level).__dict__[value_name]
+
         return val
 
     @property
     def size(self):
+        "Returns the size of the character"
         return self.race.size
 
     @property
-    def fortSave(self):
-        return self._getClassValue('fortSave') + self.mod('con')
+    def fort_save(self):
+        "The base fort save"
+        return self._get_class_value('fort_save') + self.mod('con')
 
     @property
-    def refSave(self):
-        return self._getClassValue('refSave') + self.mod('dex')
+    def ref_save(self):
+        "The base-reflex save"
+        return self._get_class_value('ref_save') + self.mod('dex')
 
     @property
-    def willSave(self):
-        return self._getClassValue('willSave') + self.mod('wis')
+    def will_save(self):
+        "The base-will save"
+        return self._get_class_value('will_save') + self.mod('wis')
 
     @property
-    def baseAttack(self):
-        return self._getClassValue('baseAttack')
+    def base_attack(self):
+        "Base attack property - just the summation of the class scores"
+        return self._get_class_value('base_attack')
 
 
